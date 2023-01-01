624b364767db3b2e3fde2fd0527c8f72191d2a16
McGrath <heather.mcgrath@nrcan-rncan.gc.ca>
2022-12-31 20:34:53 -0500

    updated

--- RunoffPred.js
+++ RunoffPred.js
@@ -1,7 +1,7 @@
  /*************************
 * Add OpenStreetMap map using Leaflet
          ************************/
-		const mymap = L.map("mapid").setView([43.356, -80.316], 13);
+		const mymap = L.map("mapid").setView([43.356, -80.316], 6);
 		L.tileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png", {
 			maxZoom: 19,
 			attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
@@ -22,8 +22,7 @@
 		
 		//Create a GeoJSON layer
 		var geojsonLayer = new L.GeoJSON(geojsonLayer, {
-			style: myStyle,
-			onEachFeature: onEachFeature
+			style: myStyle
 		}).addTo(mymap);
 
 		//Request the GeoJson data from GitHub using Ajax
